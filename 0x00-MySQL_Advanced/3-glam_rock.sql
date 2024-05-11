f733d735d7364055abd83238042b4ef3afe0fa57
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-11 23:38:55 +0300

    feat: SQL script that lists all bands with Glam rock as their main style, ranked by their longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Script that ranks country origins of bands, ordered by the number of (non-unique) fans
+
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam Rock%'
+ORDER BY lifespan DESC;
