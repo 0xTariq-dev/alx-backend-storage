d2159a9b82b8114fc4a8b36ac74688373f480926
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-14 23:18:39 +0300

    feat: Nginx logs query script #3

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
-"""
+'''
 Module contains Python script that provides some stats about Nginx logs
 stored in MongoDB
-"""
+'''
 from pymongo import MongoClient
 
 
 def filter_nginx_logs(nginx_logs):
-    """Filters nginx logs output from database collecion"""
-    print(f"{nginx_logs.count_documents({})} logs")
-    print("Methods:")
+    '''Filters nginx logs output from database collecion'''
+
+    print('{} logs'.format(nginx_logs.count_documents({})))
+    print('Methods:')
+
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        method_count = len(list(nginx_logs.find({"method": method})))
-        print(f"\tmethod {method}: {method_count}")
-    status_route_visits = len(list(
-        nginx_logs.find({'method': 'GET', 'path': '/status'})
-        ))
-    print(f'{status_route_visits} status check')
+        method_count = len(list(nginx_logs.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, method_count))
+
+    filters = {'method': 'GET', 'path': '/status'}
+    status_route_visits = len(list(nginx_logs.find(filters)))
+    print('{} status check'.format(status_route_visits))
+
 
 
 def run():
-    """Configure Client and query logs data"""
+    '''Configure Client and query logs data'''
     client = MongoClient('mongodb://127.0.0.1:27017')
     filter_nginx_logs(client.logs.nginx)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
