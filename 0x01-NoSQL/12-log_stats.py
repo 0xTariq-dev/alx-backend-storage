a88e204696432859394478d6a8f7105adb6353ed
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-14 23:00:21 +0300

    feat: Nginx logs query script #1

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,12 +4,19 @@ stored in MongoDB"""
 from pymongo import MongoClient
 
 
+def filter_nginx_logs(nginx_logs):
+    """Filters nginx logs output from database collecion"""
+    print(f"{nginx_logs.count_documents({})} logs")
+    print("Methods:")
+    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+        method_count = len(list(nginx_logs.find({"method": method})))
+        print(f"\tmethod {method}: {method_count}")
+    status_route_visits = len(list(
+        nginx_logs.find({'method': 'GET', 'path': '/status'})
+        ))
+    print(f'{status_route_visits} status check')
+
+
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-
-    print(f"{nginx_logs.count_documents({})} logs\nMethods:")
-    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
-        method_count = nginx_logs.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_count}")
-    print(f'{nginx_logs.count_documents({"path": "/status"})} status check')
+    filter_nginx_logs(client.logs.nginx)
