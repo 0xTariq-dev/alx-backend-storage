50f19c8c5191ac66c8819529b3acb30f5c737cb0
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-14 23:11:04 +0300

    feat: Nginx logs query script #2

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""Module contains Python script that provides some stats about Nginx logs
-stored in MongoDB"""
+"""
+Module contains Python script that provides some stats about Nginx logs
+stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
@@ -8,7 +10,8 @@ def filter_nginx_logs(nginx_logs):
     """Filters nginx logs output from database collecion"""
     print(f"{nginx_logs.count_documents({})} logs")
     print("Methods:")
-    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
         method_count = len(list(nginx_logs.find({"method": method})))
         print(f"\tmethod {method}: {method_count}")
     status_route_visits = len(list(
@@ -17,6 +20,11 @@ def filter_nginx_logs(nginx_logs):
     print(f'{status_route_visits} status check')
 
 
-if __name__ == "__main__":
+def run():
+    """Configure Client and query logs data"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     filter_nginx_logs(client.logs.nginx)
+
+
+if __name__ == "__main__":
+    run()
