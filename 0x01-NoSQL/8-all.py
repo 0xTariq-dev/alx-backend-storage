ee296fb367301867a9487c446596e90d6884cf0b
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-14 17:50:41 +0300

    fix: Python function that lists all documents in a collection #1

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """Module contains Python function that lists all documents in a collection"""
 
-from pymongo.collection import Collection
-from typing import List, Dict
 
-
-def list_all(mongo_collection: Collection) -> List[Dict]:
+def list_all(mongo_collection):
     """Lists all documents in a mongodb collection"""
     return [doc for doc in mongo_collection.find()]
