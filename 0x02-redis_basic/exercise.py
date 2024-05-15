9408d1bff4ef1f0a807aa43a25b3e35ee71da39c
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-16 00:57:28 +0300

    feat: Task #1 'Reading from Redis and recovering original type'

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 from uuid import uuid4 as uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,19 @@ class Cache:
         key = str(uuid())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        """Get the data from the database."""
+        call = self._redis.get(key)
+        return fn(call) if fn is not None else call
+
+    def get_str(self, key: str) -> str:
+        """Get the data from the database as a string."""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Get the data from the database as an integer."""
+        return self.get(key, lambda x: int(x))
